9aae9cab36c9b42b8bb6e89393c98d10c137b4e6
iulianpro <iulian.prodea@gmail.com>
2020-01-17 02:24:39 +0000

    added function and specifications to test

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -1,7 +1,15 @@
-describe('Calculator', function() {
-    describe('Addition test', function() {
-        it('should return 42', function() {
+describe('Calculator', function () {
+    describe('Addition test', function () {
+        it('should return 42', function () {
             expect(addition(20, 22)).toBe(42);
-        })
-    })
+        });
+        it('should return 26', function () {
+            expect(addition(7, 19)).toBe(26);
+        });
+        it('should return an error if we dont supply two numbers', function () {
+            spyOn(window, 'alert');
+            addition('Hitchhikers', 'Guide');
+            expect(window.alert).toHaveBeenCalledWith('Error!');
+        });
+    });
 });
